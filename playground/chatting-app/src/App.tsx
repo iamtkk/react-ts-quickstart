4de0148c068a219607a89409162fae4adacb6203
김태관 <iamtk@neowiz.com>
2023-01-09 19:46:31 +0900

    chatting-app 추가

---
+++ playground/chatting-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import Chatting from './Chatting';
+
+export default class App extends Component<{}, {}> {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Chatting />
+      </div>
+    );
+  }
+}
