88c3c3fab6d2b2fcfb8febc6073d60f1e7b2fc8d
김태관 <iamtk@neowiz.com>
2023-01-31 12:08:30 +0900

    todilist router에 axios 적용전

--- playground/axios-test-app/src/main.tsx
+++ playground/axios-test-app/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App4';
+import App from './App6';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
